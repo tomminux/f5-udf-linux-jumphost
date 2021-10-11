6e8b4ef0e09864e03832df669ae96bfe196a0eba
Paolo Arcagni <paolo.arcagni@gmail.com>
2021-10-11 08:10:08 +0200

    oct 11 2021 - 1

--- init-scripts/1.linux-jumphost.sh
+++ init-scripts/1.linux-jumphost.sh
@@ -38,13 +38,6 @@ sudo /bin/bash -c 'echo -e "user\nuser" | passwd user'
 
 DEBIAN_FRONTEND=noninteractive
 sudo apt update 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common ansible
 #sudo apt upgrade -y
 #sudo apt autoremove
-
-## --> Ansible installation
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-add-repository "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main"
-DEBIAN_FRONTEND=noninteractive
-sudo apt update
-sudo apt install ansible -y
